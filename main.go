4105c60ce130331f8df5d93ee2240775ad417b32
Sebastien Binet <binet@cern.ch>
2017-01-16 18:19:16 +0100

    tcp-srv: add SHT3x data collection

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/go-daq/smbus"
+	"github.com/go-daq/smbus/sensor/sht3x"
 	"github.com/go-daq/smbus/sensor/tsl2591"
 )
 
@@ -64,6 +65,7 @@ func handle(conn net.Conn) {
 	err = json.NewEncoder(conn).Encode(data)
 	if err != nil {
 		log.Printf("error sending json data: %v\n", err)
+		return
 	}
 }
 
@@ -96,23 +98,23 @@ type Sensors struct {
 
 func (s *Sensors) read(bus *smbus.Conn, addr uint8) error {
 	var err error
-	err = s.Tsl.read(bus, addr, 0x0)
+	err = s.Tsl.read(bus, addr, 0x01)
 	if err != nil {
 		return err
 	}
-	err = s.Sht31.read(bus, addr, 0x1)
+	err = s.Sht31.read(bus, addr, 0x02)
 	if err != nil {
 		return err
 	}
-	err = s.Si7021[0].read(bus, addr, 0x2)
+	err = s.Si7021[0].read(bus, addr, 0x04)
 	if err != nil {
 		return err
 	}
-	err = s.Si7021[1].read(bus, addr, 0x3)
+	err = s.Si7021[1].read(bus, addr, 0x08)
 	if err != nil {
 		return err
 	}
-	err = s.Bme.read(bus, addr, 0x4)
+	err = s.Bme.read(bus, addr, 0x10)
 	if err != nil {
 		return err
 	}
@@ -125,8 +127,12 @@ type Tsl struct {
 	IR   uint16  `json:"ir"`
 }
 
-func (tsl *Tsl) read(bus *smbus.Conn, addr uint8, ch int) error {
-	var err error
+func (tsl *Tsl) read(bus *smbus.Conn, addr uint8, ch uint8) error {
+	err := bus.WriteReg(addr, 0x04, ch)
+	if err != nil {
+		return err
+	}
+
 	dev, err := tsl2591.Open(bus, tsl2591.Addr, tsl2591.IntegTime100ms, tsl2591.GainLow)
 	if err != nil {
 		return err
@@ -149,9 +155,31 @@ type Sht31 struct {
 	Hum  float64 `json:"hum"`
 }
 
-func (sht *Sht31) read(bus *smbus.Conn, addr uint8, ch int) error {
-	var err error
-	return err
+func (sht *Sht31) read(bus *smbus.Conn, addr uint8, ch uint8) error {
+	err := bus.WriteReg(addr, 0x04, ch)
+	if err != nil {
+		return err
+	}
+
+	dev, err := sht3x.Open(bus, sht3x.I2CAddr)
+	if err != nil {
+		return err
+	}
+
+	t, rh, err := dev.Sample()
+	if err != nil {
+		return err
+	}
+
+	sht.Temp = t
+	sht.Hum = rh
+
+	err = dev.ClearStatus()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 type Si7021 struct {
@@ -159,8 +187,12 @@ type Si7021 struct {
 	Hum  float64 `json:"hum"`
 }
 
-func (si *Si7021) read(bus *smbus.Conn, addr uint8, ch int) error {
-	var err error
+func (si *Si7021) read(bus *smbus.Conn, addr uint8, ch uint8) error {
+	err := bus.WriteReg(addr, 0x04, ch)
+	if err != nil {
+		return err
+	}
+
 	return err
 }
 
@@ -170,8 +202,12 @@ type Bme struct {
 	Pres float64 `json:"pres"`
 }
 
-func (bme *Bme) read(bus *smbus.Conn, addr uint8, ch int) error {
-	var err error
+func (bme *Bme) read(bus *smbus.Conn, addr uint8, ch uint8) error {
+	err := bus.WriteReg(addr, 0x04, ch)
+	if err != nil {
+		return err
+	}
+
 	return err
 }
 
